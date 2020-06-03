2b019f2148441648efc30fdb075cced699fe0633
Guilherme Amado <guiii@live.com>
2020-06-03 00:02:24 -0300

    Dia 2 - Adicionado logica em javascript para popular estados e cidades no form

---
+++ scripts/create-point.js
@@ -0,0 +1,40 @@
+const IBGE_API_URL = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+
+
+function populateUFs() {
+    const ufSelect = document.querySelector('select[name=uf]');
+
+    fetch(IBGE_API_URL)
+        .then(res => res.json())
+        .then(ufs => {
+            for (const uf of ufs) {
+
+                ufSelect.innerHTML += `<option value="${uf.id}">${uf.nome}</option>`
+            }
+
+        })
+}
+
+function getCities(event) {
+    const citySelect = document.querySelector('select[name=city]');
+    const ufID = event.target.value;
+    const url = `${IBGE_API_URL}/${ufID}/municipios`
+
+    fetch(url)
+        .then(res => res.json())
+        .then(cities => {
+            for (const city of cities) {
+
+                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            }
+
+            citySelect.disabled = false;
+
+        })
+}
+
+populateUFs()
+
+document
+    .querySelector('select[name=uf]')
+    .addEventListener('change', getCities);
