65c8a2283d6e14a680343341730c397245865a56
Guilherme Amado <guiii@live.com>
2020-06-03 15:14:26 -0300

    Dia 3 - Atualizada forma de resolver bug das cidades

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,45 +1,41 @@
-const IBGE_API_URL = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
-
+const IBGE_API_URL =
+  "https://servicodados.ibge.gov.br/api/v1/localidades/estados";
 
 function populateUFs() {
-    const ufSelect = document.querySelector('select[name=uf]');
+  const ufSelect = document.querySelector("select[name=uf]");
 
-    fetch(IBGE_API_URL)
-        .then(res => res.json())
-        .then(ufs => {
-            for (const uf of ufs) {
-
-                ufSelect.innerHTML += `<option value="${uf.id}">${uf.nome}</option>`
-            }
-
-        })
+  fetch(IBGE_API_URL)
+    .then((res) => res.json())
+    .then((ufs) => {
+      for (const uf of ufs) {
+        ufSelect.innerHTML += `<option value="${uf.id}">${uf.nome}</option>`;
+      }
+    });
 }
 
 function getCities(event) {
-    const citySelect = document.querySelector('select[name=city]');
-    const stateInput = document.querySelector('input[name=state]');
-    const ufID = event.target.value;
-    const url = `${IBGE_API_URL}/${ufID}/municipios`
+  const citySelect = document.querySelector("select[name=city]");
+  const stateInput = document.querySelector("input[name=state]");
+  const ufID = event.target.value;
+  const url = `${IBGE_API_URL}/${ufID}/municipios`;
 
-    const indexOfSelectedState = event.target.selectedIndex;
-    stateInput.value = event.target.options[indexOfSelectedState].text;
+  const indexOfSelectedState = event.target.selectedIndex;
+  stateInput.value = event.target.options[indexOfSelectedState].text;
 
-    fetch(url)
-        .then(res => res.json())
-        .then(cities => {
-            citySelect.innerHTML = ''
-            for (const city of cities) {
+  citySelect.innerHTML = "<option value>Selecione a Cidade</option>";
+  citySelect.disabled = true;
 
-                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
-            }
+  fetch(url)
+    .then((res) => res.json())
+    .then((cities) => {
+      for (const city of cities) {
+        citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+      }
 
-            citySelect.disabled = false;
-
-        })
+      citySelect.disabled = false;
+    });
 }
 
-populateUFs()
+populateUFs();
 
-document
-    .querySelector('select[name=uf]')
-    .addEventListener('change', getCities);
+document.querySelector("select[name=uf]").addEventListener("change", getCities);
