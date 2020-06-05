1b97093ef14ea97d6f90a529b3ec834e815c8768
Guilherme Amado <guiii@live.com>
2020-06-05 02:25:00 -0300

    Dia 4 - Template engine (nunjucks), estrutura e organização HTML

--- src/server.js
+++ src/server.js
@@ -4,18 +4,25 @@ const server = express();
 // configurar pasta publica
 server.use(express.static("public/"));
 
+// utilizando template engine
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true,
+});
+
 // configurar rotas da aplicação
 // pagina inicial
 server.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  return res.render("index.html");
 });
 // cadastro ponto de coleta
 server.get("/create-point", (req, res) => {
-  res.sendFile(__dirname + "/views/create-point.html");
+  return res.render("create-point.html");
 });
 // resultados da pesquisa
 server.get("/search-results", (req, res) => {
-  res.sendFile(__dirname + "/views/search-results.html");
+  return res.render("search-results.html");
 });
 
 // ligar o servidor
